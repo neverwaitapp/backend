47c731589cc92e6131f39e312c8d157286c44817
Louis Gallet <louis.gallet@tumo.world>
2022-06-19 20:43:17 +0200

    feat: :construction:  Change parameters in eventmodel

--- src/models/bookingModel.ts
+++ src/models/bookingModel.ts
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import { isPropertyAssignment } from "typescript";
 
 export interface BookingModel extends mongoose.Document {
     eventID: string;
@@ -6,6 +7,7 @@ export interface BookingModel extends mongoose.Document {
     email: string;
     phone: string;
     guests: number;
+    isPassed: Boolean;
 }
 
 const bookingSchema = new mongoose.Schema({
@@ -13,7 +15,8 @@ const bookingSchema = new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true },
     phone: { type: String, required: true },
-    guests: { type: Number, required: true, default: 1, min: 1, max: 2 }
+    guests: { type: Number, required: true, default: 1, min: 1, max: 2 },
+    isPassed: { type: Boolean, required: false, default: false }
 });
 
 export const Booking = mongoose.model<BookingModel>("Booking", bookingSchema);
--- src/models/eventModel.ts
+++ src/models/eventModel.ts
@@ -5,13 +5,15 @@ export interface EventModel extends mongoose.Document {
     description: string;
     date: Date;
     location: string;
+    eventProperties: number[];
 }
 
 const eventSchema = new mongoose.Schema({
     name: { type: String, required: true },
     description: { type: String, required: true },
     date: { type: Date, required: true },
-    location: { type: String, required: true }
+    location: { type: String, required: true },
+    eventProperties: { type: [String], required: true}
 });
 
 export const Event = mongoose.model<EventModel>("Event", eventSchema);
